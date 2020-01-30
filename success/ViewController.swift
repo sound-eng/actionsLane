77a1cc208e6928e8486c76202361727c0453df76
Oleg Naumenko <olegnowmenco@gmail.com>
2020-01-30 19:36:13 +0200

    Initial Commit

---
+++ success/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  success
+//
+//  Created by oleg.naumenko on 1/30/20.
+//  Copyright © 2020 com.avistar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
